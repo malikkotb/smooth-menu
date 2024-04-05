09b52f2d2133b8c071b118e400936db67e07e47d
Malik Kotb <malikkotb@icloud.com>
2024-04-05 07:39:38 +0200

    hover bar

--- app/page.js
+++ app/page.js
@@ -1,26 +1,35 @@
-"use client"
+"use client";
 import { useEffect } from "react";
-import gsap from "gsap/gsap-core";
+import { gsap } from "gsap";
+
+const projects = ["2022 Quiz Arena", "FoodPal Vue App", "PyTorch Image CNN", "2024 Note Taker"];
 
 export default function Home() {
+  const tl = gsap.timeline();
 
   useEffect(() => {
-    const tl = gsap.timeline()
     // const tl = gsap.timeline({defaults: { ease: "power4.inOut", duration: 0.3}})
+    // tl.to(".bar", {
+    //   width: 30,
+    // });
+  });
+
+  const hoverFunc = () => {
     tl.to(".bar", {
-      width: "100px"
-    })
-  
-  })
+      width: 30,
+    });
+  }
 
   return (
     <main className="bg-black opacity-80 h-screen m-0 flex items-center">
-        <ul className="text-white text-4xl font-thin flex flex-col gap-12">
-          <li><div className="bar bg-white h-[1px] w-0"></div><a href="#" className=" ml-16">2022 Quiz Arena</a></li>
-          <li><a href="#" className=" ml-16">2024 Note Taker</a></li>
-          <li><a href="#" className=" ml-16">FoodPal Vue App</a></li>
-          <li><a href="#" className="ml-16">PyTorch Image CNN</a></li>
-        </ul>
+      <ul className="text-white text-4xl font-thin flex flex-col gap-12">
+        {projects.map((p, index) => (
+          <li key={index} onMouseEnter={hoverFunc}>
+            <div className="bar"></div>
+            <a href="#" className=" ">{p}</a>
+          </li>
+        ))}
+      </ul>
     </main>
   );
 }
