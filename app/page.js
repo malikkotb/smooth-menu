6a34baf1f7c2621762c5ed7098fe32ad4ea0b73d
Malik Kotb <malikkotb@icloud.com>
2024-04-06 08:41:27 +0200

    foreach timeline

--- app/page.js
+++ app/page.js
@@ -1,32 +1,55 @@
 "use client";
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 import { gsap } from "gsap";
 
-const projects = ["2022 Quiz Arena", "FoodPal Vue App", "PyTorch Image CNN", "2024 Note Taker"];
+const projects = [
+  { title: "2022 Quiz Arena", tag: "AR 2022" },
+  { title: "Pastry Shop", tag: "UI/UX 2023" },
+  { title: "PyTorch Image CNN", tag: "CNN 2023" },
+  { title: "Note Taker 2024", tag: "UI/UX 2024" },
+];
 
 export default function Home() {
-  const tl = gsap.timeline();
+  const elementsRef = useRef([]);
 
   useEffect(() => {
     // const tl = gsap.timeline({defaults: { ease: "power4.inOut", duration: 0.3}})
     // tl.to(".bar", {
     //   width: 30,
     // });
+
+    elementsRef.current.forEach((element, index) => {
+      gsap.to(element, { width: 30 });
+    });
+
+    gsap.to(".tag", {
+      y: 0,
+      delay: 0.5,
+    });
   });
 
   const hoverFunc = () => {
-    tl.to(".bar", {
-      width: 30,
-    });
-  }
+    // const tl = gsap.timeline();
+
+    // tl.to(".bar", {
+    //   width: 30,
+    // });
+  };
 
   return (
     <main className="bg-black opacity-80 h-screen m-0 flex items-center">
       <ul className="text-white text-4xl font-thin flex flex-col gap-12">
         {projects.map((p, index) => (
           <li key={index} onMouseEnter={hoverFunc}>
-            <div className="bar"></div>
-            <a href="#" className=" ">{p}</a>
+            <div className="relative flex items-center justify-between">
+              <div className="bar"></div>
+              <a href="#" className=" ">
+                {p.title}
+              </a>
+            </div>
+            <div className="mask">
+              <p className="tag">{p.tag}</p>
+            </div>
           </li>
         ))}
       </ul>
