7a6a363f667d02cc69bf601252857abc18a58e28
Malik Kotb <malikkotb@icloud.com>
2024-04-07 07:48:11 +0200

    bar animation

--- app/page.js
+++ app/page.js
@@ -12,6 +12,14 @@ const projects = [
 export default function Home() {
   const elementsRef = useRef([]);
 
+  const handleHover = (event) => {
+    gsap.to(event.target, { width: 30 });
+  };
+
+  const handleMouseOut = (event) => {
+    gsap.to(".bar", { width: 0 });
+  };
+
   useEffect(() => {
     // const tl = gsap.timeline({defaults: { ease: "power4.inOut", duration: 0.3}})
     // tl.to(".bar", {
@@ -28,31 +36,27 @@ export default function Home() {
     });
   });
 
-  const hoverFunc = () => {
-    // const tl = gsap.timeline();
-
-    // tl.to(".bar", {
-    //   width: 30,
-    // });
-  };
-
   return (
-    <main className="bg-black opacity-80 h-screen m-0 flex items-center">
-      <ul className="text-white text-4xl font-thin flex flex-col gap-12">
-        {projects.map((p, index) => (
-          <li key={index} onMouseEnter={hoverFunc}>
-            <div className="relative flex items-center justify-between">
-              <div className="bar"></div>
-              <a href="#" className=" ">
-                {p.title}
-              </a>
-            </div>
-            <div className="mask">
-              <p className="tag">{p.tag}</p>
-            </div>
-          </li>
-        ))}
-      </ul>
-    </main>
+    <div className="m-0 h-screen text-white bg-black opacity-80">
+      <main className="w-full h-screen m-0 flex items-center">
+        <section className=" flex-1">
+          <ul className="text-white text-4xl font-thin flex flex-col gap-12">
+            {projects.map((p, index) => (
+              <li key={index}>
+                <div className="relative flex items-center justify-between">
+                  <div onMouseEnter={handleHover} onMouseLeave={handleMouseOut} className="bar"></div>
+                  <a href="#" className=" ">
+                    {p.title}
+                  </a>
+                </div>
+                <div className="mask">
+                  <p className="tag">{p.tag}</p>
+                </div>
+              </li>
+            ))}
+          </ul>
+        </section>
+      </main>
+    </div>
   );
 }
