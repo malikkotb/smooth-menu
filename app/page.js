6c913eeb23a226d9c085471195021b670ea5dbbe
Malik Kotb <malikkotb@icloud.com>
2024-04-09 07:44:21 +0200

    smooth menu nav

--- app/page.js
+++ app/page.js
@@ -10,16 +10,65 @@ const projects = [
 ];
 
 export default function Home() {
-
   useEffect(() => {
+    const links = gsap.utils.toArray("li a");
 
+    links.forEach((link) => {
+      // define a gsap timeline for each of the links
+      // needs to be inside of the foreach, because
+      // if it's outside, all the links will share the same timeline
+      // -> if u hover over one link, it will animate all the other ones as well
+
+      let linkTl = gsap.timeline({ defaults: { ease: "power4.inOut", duration: 0.6 } });
+
+      const headingStart = link.querySelector(".primary");
+      const headingEnd = link.querySelector(".secondary");
+      const date = link.querySelector(".date");
+      const line = link.querySelector(".line");
+
+      linkTl
+        .to(headingStart, {
+          yPercent: -100,
+        })
+        .to(
+          headingEnd,
+          {
+            yPercent: -50,
+          },
+          // use "<" for the animations (this one and the one before to start at the same time)
+          "<"
+        )
+        .to(
+          line,
+          {
+            scaleX: 1,
+          },
+          "<"
+        )
+        .to(
+          date,
+          {
+            y: 0,
+          },
+          "<"
+        );
+
+      linkTl.pause();
+
+      link.addEventListener("mouseenter", () => {
+        linkTl.play();
+      });
+      link.addEventListener("mouseleave", () => {
+        linkTl.reverse();
+      });
+    });
   });
 
   return (
     <div className="m-0 h-screen text-white bg-black opacity-80">
       <main className="w-full h-screen m-0 flex items-center">
-        <section className=" flex-1">
-          <ul className="text-white text-4xl font-thin flex flex-col gap-12">
+        <section className="flex-1">
+          <ul className="text-white text-4xl font-thin">
             {projects.map((p, index) => (
               <li key={index}>
                 <a href="#">
@@ -27,10 +76,9 @@ export default function Home() {
                     <p className="primary">{p.title}</p>
                     <p className="secondary">{p.title}</p>
                   </div>
-                  <p>{p.tag}</p>
+                  <p className="date">{p.tag}</p>
                   <div className="line"></div>
                 </a>
-
               </li>
             ))}
           </ul>
